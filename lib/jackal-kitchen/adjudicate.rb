66844a2e0d869825b706a491fa6e0a3033f46397
Anthony Goddard <anthony@anthonygoddard.com>
2015-02-26 22:36:08 -0700

    add test, total runtime thresholds for specs

--- lib/jackal-kitchen/adjudicate.rb
+++ lib/jackal-kitchen/adjudicate.rb
@@ -79,11 +79,15 @@ module Jackal
         sorted_tests = data["examples"].sort_by{ |x|x["run_time"] }
         slowest_test = sorted_tests.last
         tests_over_threshold = []
-        threshold = config.fetch(:kitchen, :thresholds, :spec, :test_runtime, 60)
+
+        exceeded = duration > config.fetch(
+                     :kitchen, :thresholds, data["test_format"] , :total_runtime, 60
+                   )
 
         tests_over_threshold = data["examples"].reject { |e|
           if e.key?("run_time")
-            e["run_time"] < threshold
+            e["run_time"] < config.fetch(
+              :kitchen, :thresholds, data["test_format"], :test_runtime, 10)
           end
         }
 
